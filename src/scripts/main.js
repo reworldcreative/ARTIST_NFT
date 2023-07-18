b1c32423ab447873ec90dc05fcfbdfbc70970652
reworldcreative <reworldcreative@gmail.com>
2023-07-18 14:48:55 +0300

    page speed optimization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,6 @@ window.addEventListener("DOMContentLoaded", function () {
   var swiperScript = document.createElement("script");
   swiperScript.src =
     "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js";
-
-  var swiperScript = document.createElement("script");
-  swiperScript.src =
-    "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js";
   var lazyloadxt = document.createElement("script");
   lazyloadxt.src =
     "https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyloadxt/1.1.0/jquery.lazyloadxt.min.js";
